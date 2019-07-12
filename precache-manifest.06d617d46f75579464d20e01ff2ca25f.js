f5325a460a48ebf31b2df05df565355b724a9a42
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-07-12 13:01:29 +0700

    Updates

---
+++ precache-manifest.06d617d46f75579464d20e01ff2ca25f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294a8f8b399eb620cb6b269a9a02b8c6",
+    "url": "/memolang/index.html"
+  },
+  {
+    "revision": "9656ba58c2c316866f82",
+    "url": "/memolang/static/css/main.9b79af83.chunk.css"
+  },
+  {
+    "revision": "7536cf196ad92c4e51b9",
+    "url": "/memolang/static/js/2.11050585.chunk.js"
+  },
+  {
+    "revision": "9656ba58c2c316866f82",
+    "url": "/memolang/static/js/main.e677872c.chunk.js"
+  },
+  {
+    "revision": "c13bdf63035a27a0ca8d",
+    "url": "/memolang/static/js/runtime~main.ddfdec06.js"
+  }
+]);
